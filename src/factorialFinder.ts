373f03368663e59915b5b0dbf35f3dc3f3db9aba
Clinton Williams <clintonwilliams007@gmail.com>
2024-10-21 20:01:21 -0400

    created memoFactorialFinder with test

--- src/factorialFinder.ts
+++ src/factorialFinder.ts
@@ -1,14 +1,32 @@
 /* Prompt: Write a function that takes a non-negative integer and returns its factorial.
 Extension: Modify the function to handle large numbers by using memoization to improve efficiency. */
 
-
 function factorialFinder(num: number): number {
-  if(num == 0 || num ==1) {
+  if (num == 0 || num == 1) {
     return 1;
   }
-  let factorialNumber = 0;
+
+  let factorialNumber = 1;
+  for (let i = 2; i <= num; i++) {
+    factorialNumber *= i;
+  }
 
   return factorialNumber;
-};
+}
 
 export default factorialFinder;
+
+
+const memo: { [key: number] : number } = {};
+
+export function memoFactorialFinder(num: number): number {
+  if(num in memo) return memo[num];
+  
+  if(num == 0 || num == 1) {
+    memo[num] = 1;
+    return 1;
+  }
+
+  memo[num] = num * memoFactorialFinder(num -1);
+  return memo[num];
+}
--- tests/factorialFinder.test.ts
+++ tests/factorialFinder.test.ts
@@ -1,4 +1,4 @@
-import factorialFinder from '../src/factorialFinder';
+import factorialFinder, { memoFactorialFinder } from '../src/factorialFinder';
 
 test( 'the factorial of 0 is 1', () => {
   expect(factorialFinder(0)).toBe(1);
@@ -12,14 +12,50 @@ test( 'the factorial of 2 is 2', () => {
   expect(factorialFinder(2)).toBe(2);
 });
 
+test( 'the factorial of 3 is 6', () => {
+  expect(factorialFinder(3)).toBe(6);
+});
+
 test( 'the factorial of 5 is 120', () => {
   expect(factorialFinder(5)).toBe(120);
 });
 
 test( 'the factorial of 4 is 48', () => {
-  expect(factorialFinder(4)).toBe(48);
+  expect(factorialFinder(4)).toBe(24);
 });
 
 test( 'the factorial of 7 is 5040', () => {
   expect(factorialFinder(7)).toBe(5040);
 });
+
+
+
+test( 'the factorial of 0 is 1', () => {
+  expect(memoFactorialFinder(0)).toBe(1);
+});
+
+test( 'the factorial of 1 is 1', () => {
+  expect(memoFactorialFinder(1)).toBe(1);
+});
+
+test( 'the factorial of 2 is 2', () => {
+  expect(memoFactorialFinder(2)).toBe(2);
+});
+
+test( 'the factorial of 3 is 6', () => {
+  expect(memoFactorialFinder(3)).toBe(6);
+});
+
+test( 'the factorial of 5 is 120', () => {
+  expect(memoFactorialFinder(5)).toBe(120);
+});
+
+test( 'the factorial of 4 is 48', () => {
+  expect(memoFactorialFinder(4)).toBe(24);
+});
+
+test( 'the factorial of 7 is 5040', () => {
+  expect(memoFactorialFinder(7)).toBe(5040);
+});
+
+
